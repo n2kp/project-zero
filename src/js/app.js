4c21cd95b41f27e0ef8e1968a7d07f95ffcda647
Nimesh Patel <n2kp@hotmail.com>
2017-05-21 12:23:00 +0100

    More logic added

--- src/js/app.js
+++ src/js/app.js
@@ -10,46 +10,118 @@ $(() => {
   const $resetButton = $('.resetButton');
   const $nextButton = $('.nextButton');
   const $quickTime = $('.quickTime');
+  const $whichRound = $('.roundDisplay');
+  const $sound = $('.soundOnOff');
 
-  let roundNumber = 0;
-  let colorPalette = [
-                      ['#3db5f9', '#48a8e8', '#539ad7', '#5e8dc6', '#697fb5', '#7472a4', '#7e6493', '#895782', '#944a71', '#9f3c60', '#aa2f4f', '#b5213e', '#c0142d]'],
+  const colorPalette = [
+                      ['#3db5f9', '#48a8e8', '#539ad7', '#5e8dc6', '#697fb5', '#7472a4', '#7e6493', '#895782', '#944a71', '#9f3c60', '#aa2f4f', '#b5213e', '#c0142d'],
                       ['#44f267', '#4bdf68', '#51cd69', '#58ba6a', '#5ea76b', '#65956c', '#6b826d', '#726f6e', '#795d6f', '#7f4a70', '#863771', '#8c2572', '#931273']
   ];
+
+  let roundNumber = 1;
+  let $gameSquares = $('.square');
 
 
   $resetButton.hide();
   $nextButton.hide();
+  $gameSquares.hide();
+  $whichRound.hide();
 
-  $difficulty.on('click', (e) => {
-    console.log('clicked');
 
-    if ($(e.target).hasClass('easybutton')) {
-      $easyButton.hide();
-      $hardButton.hide();
-      $resetButton.show();
-      $nextButton.show();
-      alert('About to play easy');
-    } else {
-      $easyButton.hide();
-      $hardButton.hide();
-      $resetButton.show();
-      $nextButton.show();
-      alert('About to play hard');
-    }
-  });
+  function gameStart () {
+    $easyButton.hide();
+    $hardButton.hide();
+    $resetButton.show();
+    $nextButton.show();
+    $gameSquares.show();
+    $whichRound.show();
+  }
+
+
+  function colors() {
+    return colorPalette[Math.floor(Math.random()*colorPalette.length)];
+  }
+
+  let randomColorsArr = colors();
+
+  const easyR1 = [randomColorsArr[0], randomColorsArr[3], randomColorsArr[6], randomColorsArr[9], randomColorsArr[12]];
+  console.log(easyR1);
+  console.log(easyR1[0]);
+
+  let popER1 = easyR1.pop();
+  console.log(popER1);
+  let spliceER1 = easyR1.splice(0,1);
+  console.log(spliceER1);
+  typeof(spliceER1);
+
+
+
+
+
+  // $difficulty.on('click', (e) => {
+  //   console.log('clicked');
+  //
+  //   if ($(e.target).hasClass('easyButton')) {
+  //     gameStart();
+  //     alert('About to play easy');
+  //
+  //   } else if ($(e.target).hasClass('hardButton')){
+  //     gameStart();
+  //     alert('About to play hard');
+  //   }
+  // });
 
 });
 
 
-// EASY GAME
-//   easy game button hidden
-//   hard game button hidden
-//   reset button show
-//   next button show but not clickable
-//         round 1 appears
-//   random array chosen
-//   round
+// NEXT LEVEL
+//   $gameBoard.append("<div></div>").addClass('square');
+
+
+// SOUND
+// $sound.on('click', () => {
+//   $sound.src = './assets/hue_hunter_background.mp3';
+//   if ($sound.paused === false) {
+//     $sound.pause();
+//   } else {
+//     $sound.play();
+//   }
+// });
+
+// EASY MODE:
+//   Round1
+//     [0], [3], [6], [9], [12]
+//   Round 2
+//     [0], [3], [5], [7], [9], [12]
+//   Round 3
+//     [0], [1], [3], [6], [9], [11], [12]
+//   Round 4
+//     [0], [1], [3], [5], [7], [9], [11], [12]
+//   Round 5
+//     [0], [1], [3], [4], [6], [8], [9], [11], [12]
+//
+// HARD MODE:
+//   Round1
+//     [0], [1], [3], [4], [6], [8], [9], [11], [12]
+//   Round 2
+//     [0], [1], [2], [4], [5], [7], [8], [10], [11], [12]
+//   Round 3
+//     [0], [1], [2], [3], [5], [6], [7], [9], [10], [11], [12]
+//   Round 4
+//     [0], [1], [2], [3], [4], [5], [7], [8], [9], [10], [11], [12]
+//   Round 5
+//     [0], [1], [2], [3], [4], [5], [6], [7], [8], [9], [10], [11], [12]
+
+
+
+
+
+
+
+
+
+
+
 
 
 
