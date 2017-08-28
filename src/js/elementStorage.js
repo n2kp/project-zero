bfa761d5713bd5f3efd8450a8226e38a0540c7bc
Nimesh Patel <n2kp@hotmail.com>
2017-08-28 19:31:40 +0100

    Palettes and Sequences removed from app.js and inserted into new js files

---
+++ src/js/elementStorage.js
@@ -0,0 +1,12 @@
+const elementSelector = [
+  [0, 3, 6, 9, 12],
+  [0, 3, 5, 7, 9, 12],
+  [0, 1, 3, 6, 9, 11, 12],
+  [0, 1, 3, 5, 7, 9, 11, 12],
+  [0, 1, 3, 4, 6, 8, 9, 11, 12],
+  [0, 1, 2, 4, 5, 7, 8, 10, 11, 12],
+  [0, 1, 2, 3, 5, 6, 7, 9, 10, 11, 12],
+  [0, 1, 2, 3, 4, 5, 7, 8, 9, 10, 11, 12],
+  [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+];
+// Contains 9 arrays with varying indices, used to determine how many indices to deploy depending upon which round and what game mode.
